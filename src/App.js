cbf2a2694fe1da43ac52456dc34d86a7c6b5a98f
mateopedehonta <mateopedehonta@gmail.com>
2022-02-23 11:56:35 -0300

    navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import navBar from './components/navBar'
+import'./components/navBar.js'
 import './App.css';
 
 function App() {
   return <>
     <navBar />
-     
+    <nav>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><a href='#'>About us</a></li>
+        <li><a href='#'>Login</a></li>
+      </ul>
+    </nav>
   </>
 }
 
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,7 +1,7 @@
 function navBar() {
-    return <>
-        <h1>hola mundo</h1>      
-    </>
+    return(
+        <h1>hola mundo</h1>
+    );
 }
 export default navBar;
   
